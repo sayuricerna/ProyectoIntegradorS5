1448bd7aa9f4055fe644bb3416cb100e623f71a8
Sayuri Melina <cernasayuri@gmail.com>
2025-07-04 07:01:17 -0500

    auth admin y user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // se crea nuevo usuario
+            $table->string('utype')->default('USR')->comment('ADM for Admin and USR for user or custumer');
             $table->rememberToken();
             $table->timestamps();
         });
